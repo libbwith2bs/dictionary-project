ef64fe3d3c606a92804a1b6b1150d3bf1a80167f
libbwith2bs <100886874+libbwith2bs@users.noreply.github.com>
2022-08-31 15:56:52 -0500

    Synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,10 +1,19 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h4>{props.meaning.partOfSpeech}</h4>
-      <p>{props.meaning.definitions[0].definition}</p>
+      <p>
+        <strong>Definition: </strong>
+        {props.meaning.definitions[0].definition}
+        <br />
+        <strong>Example: </strong>
+        {props.meaning.definitions[0].example}
+        <br />
+        <Synonyms synonyms={props.meaning.synonyms} />
+      </p>
     </div>
   );
 }
